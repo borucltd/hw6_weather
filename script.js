8a73cc2cc3f4fb2e6ed4da9c226a73f110c07468
Mariusz Boruc <mariuszbrc@gmail.com>
2020-03-29 23:05:14 +1030

    API works, creating DOM ok. Local storage ok.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
-                                                                                           
+              
+// Really cool stuff to change cursor when running API ajax calls
+$(document).ajaxStart(function ()
+{
+    $('body').addClass('wait');
+
+}).ajaxComplete(function () {
+
+    $('body').removeClass('wait');
+
+});
+
+
+
+
 // Local Storage
+// Check if it is defined
 let lastSearch = localStorage.getItem('lastCity');
 if (lastSearch === null ) {
     console.log("Local storage not initiated.");
@@ -25,14 +40,59 @@ function frenderWeather(city){
                 url: queryUrl,
                 method: 'GET',
                 success: function(weatherResponse){
-                    console.log(weatherResponse);
+                    // when we have successful response
+                    // create DOM if it doesn't exists
+                    if ($('#button'+city).length === 0) {
+
+                        // new button for the city
+                        // each button has ID = btncity
+                        // each button has CUSTOM class city used later
+                        let newButton = $('<button>');
+                        newButton.attr('id', "btn"+city);
+                        newButton.attr('type', 'button');
+                        newButton.addClass('btn btn-primary m-1 city');
+                        
+                        newButton.text(city);
+
+                        // add button to citiesArea
+                        $("#citiesArea").append(newButton);
+
+                        // add city to local storage
+                        localStorage.setItem('lastCity',city);                    }
+
+                        // display weather
+                        console.log(weatherResponse);
+
+                        // add event listener to buttons with class city
+                        $(".city").on( "click", function() {
+                            console.log("Running API");
+                        });
                 },
                 error: function(){
                     console.log("API call: something went wrong.");
                 }
 
             });
+            
 }
+
+
+// Add city to history 
+// each save button  on click saves to local storage
+$("#searchButton").on( "click", function() {
+    // Read city name
+    let city = $("#searchCity").val();
+
+    if (city === "" ) {
+        // We don't want to make API calls which will not work 100%
+        console.log("EMPTY !!!");
+    } else {
+        // make API call
+        frenderWeather(city);       
+    }
+});
+
+
 
 
 })
