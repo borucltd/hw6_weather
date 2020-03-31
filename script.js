89b5a89d577746a65ef6b0424d73cee3d1c62a71
Mariusz Boruc <mariuszbrc@gmail.com>
2020-03-31 23:57:46 +1030

    Added UV index.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 // Really cool stuff (googled) to change cursor when running API ajax calls
 // Each time we start ANY ajax call
 // we add class wait to the body
+// ajaxStart is global handler, same as ajaxComplete
 $(document).ajaxStart(function ()
 {
     $('body').addClass('wait');
@@ -20,11 +21,11 @@ let lastSearch = localStorage.getItem('lastCity');
 if (lastSearch === null ) {
     console.log("Local storage not initiated.");
 } else {
-    frenderWeather(lastSearch);
+    frenderWeather(lastSearch,false);
 }
 
 // Displays weather
-function frenderWeather(city){
+function frenderWeather(city,skip){
 
     // Open Weather API
     const apiKey = 'd6c3151200124f6b3dfdd52227d4e82c';
@@ -33,8 +34,12 @@ function frenderWeather(city){
     let apiQuery = '&appid=' + apiKey;
     let unitsQuery = '&units=metric';
     
-    //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
     let queryUrl = urlApi + cityQuery + apiQuery + unitsQuery;
+
+    let updateDOM = skip;
+
+    console.log(updateDOM);
        
     // make API call
     $.ajax({
@@ -42,10 +47,10 @@ function frenderWeather(city){
             method: 'GET',
 
             success: function(response) {
-                // Update DOM
-                // create DOM if it doesn't exists
-                console.log($(this).text);
-                if ($('#button'+city).length === 0 ) {
+                // Because API call can be triggered by clicking search button
+                // or button with the city name
+                // we need to check when we add new button                
+                if ($('#button'+city).length === 0 && updateDOM === false ) {
 
                     let newButton = $('<button>');
                     newButton.attr('id', "btn"+city);
@@ -58,35 +63,40 @@ function frenderWeather(city){
 
                     // add city to local storage as last
                     localStorage.setItem('lastCity',city);
-                }    
 
-                // add event listener to buttons with class city
-                // we use off/on to run this once only
-                // in addition to that we add event.preventDefault
-                // this is becasue we invocate click event per class and not id
-                $(".city").off().on( "click", function() {
-                    event.preventDefault();
-                    // make API call
-                    frenderWeather($(this).text()); 
-                });
-            
+                    // add event listener to buttons with class city
+                    // we use off/on to run this once only
+                    // in addition to that we add event.preventDefault
+                    // this is becasue we invocate click event per class and not id
+                    $(".city").off().on( "click", function() {
+                        event.preventDefault();
+                        // make API call
+                        frenderWeather($(this).text(),true); 
+                    });
+                }                 
+                       
             console.log(response);
-           
-            // display weather
+            // use resposne to print weather
             let cityName = response.city.name;
+            // starting same for each day
             let hour = ' 15:00:00';
-            // seconds, difference between GMT and Adelaide
-            // I could use timeoffset from the API call but had no power to thing about it...
+            // offset specific to Adelaide ()
             let timeshift = 5400; 
+            // pick up the actual date
             let today = moment().format('YYYY-MM-DD');
             // here we will have 6 unix timestamps for 6 days
-            // each timestamp is for 3 PM
+            // today and 5 next days
+            // each timestamp is for 3 PM + timeshift
+            // by doing this we will obtian weather for the time of the day
             let timestamps = [];
             let day;
             
             for (let i = 0; i<6; i ++){
+
+                // calculate date of the subsequent day + add 15:00:00
                 day = moment().add(i,'days').format('YYYY-MM-DD') + hour;
-                timestamps[i] = moment(day).unix() + timeshift;
+                // calculate unix timestamp + timeshift (SECONDS)
+                timestamps[i] = day;
 
                 // today
                 if (i===0) {
@@ -97,37 +107,46 @@ function frenderWeather(city){
                     let todayTemperature = $("<p>");
                     let todayHumidity = $("<p>");
                     let todayWind = $("<p>");
-                    let todayUV = $("<p>");
+                   
 
-                    todayHeader.text(cityName + " " + moment.unix(timestamps[i]).format('DD/MM/YYYY'));
-                    todayTemperature.text("Temperature:");
-                    todayHumidity.text("Humidity:");
-                    todayWind.text("Wind:");
-                    todayUV.text("UV:");
-
+                    todayHeader.text(cityName + " " + timestamps[i]);
+                    // response.cnt is the number of results from API
+                    // by default this is 40
+                    // we need to find result for our timestamp
                     for (let j = 0; j < response.cnt; j++) 
                     {
-                        
-                        if (response.list[j].dt === timestamps[i]) {
-                            console.log("HERRE");
-                            todayTemperature.text("Temp.: " + response.list[j].main.temp +   String.fromCharCode(176) + "C");
+                        // responde.X.dt is timestamp in GMT
+                        // timestamp is timestamp in GMT
+                        console.log(response.list[j].dt_txt + " ===  " + timestamps[i]);
+                        if (response.list[j].dt_txt === timestamps[i]) {
+                            
+                            todayTemperature.text("Temperature: " + response.list[j].main.temp +   String.fromCharCode(176) + "C");
                             todayHumidity.text("Humidity: " + response.list[j].main.humidity + " %");
                             todayWind.text("Wind: " + response.list[j].wind.speed + " mps " );
-                            todayUV.text("UV: ");
-                            break
+                            // run OpenUV API 
+                            //getUV(lat,lng,dt);
+                            let timeUV = timestamps[i].replace(/\s/g,'T');
+                            console.log(response.city.coord.lat);
+                            console.log(response.city.coord.lon);
+                            console.log(timeUV);
+                            $("#todayWeather").append(todayHeader);
+                            $("#todayWeather").append(todayTemperature);
+                            $("#todayWeather").append(todayHumidity);
+                            $("#todayWeather").append(todayWind);
 
+                            // check UV and add to DOM
+                            getUV(response.city.coord.lat,response.city.coord.lon,timeUV);
+                                                  
+                            // this means we found weather for to today for time 15:00:00
+                            // we can break from the loop
+                            break;
                         }
-                    }                 
-                    
-                    $("#todayWeather").append(todayHeader);
-                    $("#todayWeather").append(todayTemperature);
-                    $("#todayWeather").append(todayHumidity);
-                    $("#todayWeather").append(todayWind);
-                    $("#todayWeather").append(todayUV);
+                    }                                  
 
                 } else {
                     // next days
                     if (i === 1) {
+                        // remove all DOM elements from the previous city
                         $("#nextDaysWeather").empty();
                     }
                    
@@ -137,7 +156,7 @@ function frenderWeather(city){
                     let nextdayTemperature = $("<p>");
                     let nextdayHumidity = $("<p>");
 
-                    nextdayDate.text(moment.unix(timestamps[i]).format('DD/MM/YYYY'));
+                    nextdayDate.text(timestamps[i].split(" ",1));
                     nextdayTemperature.text("Temp.: " + response.list[i].main.temp +  String.fromCharCode(176) + "C");
                     nextdayHumidity.text("Humidity: " + response.list[i].main.humidity + " %");
 
@@ -149,11 +168,9 @@ function frenderWeather(city){
 
                 }
 
-            }
-            // display weather for the next days
-
-            // console.log(weatherResponse);       
+            }     
             },
+
             error: function(){
                 // write to console
                 console.log("API call: something went wrong.");
@@ -162,22 +179,69 @@ function frenderWeather(city){
             
 }
 
-// When search is clicked
+// search button on click
 $("#searchButton").on( "click", function() {
+
     event.preventDefault();
-    // Read city name
+
+    // read city name
     let city = $("#searchCity").val();
 
     // if city is empty we do nothing
     if (city === "" ) {
+
         // We don't want to make API calls which will not work 100%
-        console.log("Empty city !!!");
         $("#searchCity").addClass("bg-danger");
+
     } else {
+
         // once there is city provided we can fire up api
         $("#searchCity").removeClass("bg-danger");
-        frenderWeather(city);  
+        // provide city name
+        // second argument means we will create DOM
+        frenderWeather(city,false);  
     }
 });
+
+
+// we need to use different API to get UV index
+function getUV(lat,lng,dt) {
+
+    let urlUV = 'https://api.openuv.io/api/v1/uv';
+    let tokenUV = '1c5c77e7762002273011c3361fd25485';
+    let indexUV;
+    let latUV = lat;
+    let lngUV = lng;
+    let dtUV = dt;
+ 
+    $.ajax({
+        // break async in order to wait for API...
+        url: 'https://api.openuv.io/api/v1/uv?lat=' + latUV + '&lng=' + lngUV + '&dt=' + dtUV,
+        type: 'GET',
+        dataType: 'json',
+        // token needs to be send inside the header
+        beforeSend: function(request) {
+          request.setRequestHeader('x-access-token', tokenUV);
+        },
+        success: function(response) {
+            console.log(response);
+            indexUV = response.result.uv_max;
+            let todayUV = $("<p>");
+            todayUV.text("UV: " + indexUV);
+            $("#todayWeather").append(todayUV);
+            
+        },
+        error: function(response) {
+            let todayUV = $("<p>");
+            todayUV.text("UV: " + "N/A");
+            $("#todayWeather").append(todayUV);
+        }
+    });
+
+
+   
+
+    
+}
 
 })
